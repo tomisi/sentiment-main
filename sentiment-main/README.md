7519475fbe0d9716e12ffda8da72c9f3cbccf970
Bamidele Oluwatominsin <65241457+tomisi@users.noreply.github.com>
2024-09-16 14:16:58 +0100

    Initial sentiment analysis project commit

---
+++ sentiment-main/README.md
@@ -0,0 +1,41 @@
+### Project Setup Instructions
+
+- Create a Virtual Environment using either Venv/ Conda
+- Activate the Environment and  run ```pip install -r requirements.txt```
+
+1. **PyTorch Library Installation:**
+   - If PyTorch Installation fails via `requirements.txt`, please visit the [Official PyTorch Page](https://pytorch.org/) and follow the installation instructions. It is recommended to install PyTorch without CUDA support.
+
+2. **Setting Up Secrets for Local Run:**
+   - Create a `secrets.toml` file in the `./streamlit` directory within your project root folder. 
+   
+   - The structure of the `secrets.toml` file should be as follows:
+
+     ```toml
+     [openai]
+     api_key = "Your API KEY"
+     ```
+
+     Replace `"Your API KEY"` with your actual OpenAI API Key. This file will be used to store sensitive information securely within your project.
+
+
+3. **System Error Handling:**
+
+   - The system verifies if the uploaded file conforms to the required file structure, notifying businesses about any missing columns.
+   - Automatically detecting file encodings, the system ensures that files are read based on these encodings.
+   - Date columns are converted to the required date format by the system.
+   - To prevent key errors caused by columns specified in different formats, the system standardizes all columns to lowercase.
+
+     ```
+     streamlit run sentiment.py
+     ```
+     
+### Project Testing
+
+  - Download Flipkart Customer Product Reviews Data found in the `Dataset` Folder.
+  - Upload the Dataset to the sentiment Page to generate the Sentiment Scores and Overall Sentiments for each of the Customer Reviews.
+  - Download the Updated Reviews( e.g `Flipkart_updated_reviews_2019_11_01.csv`) and store them Locally.
+  - Upload the Updated Reviews to Business Recommendations Page to get actionable recommendations from `gpt3.5-turbo` model and ask follow-up questions.
+  - Upload the Updated Reviews to Data Insights to generate Insights such as Product Vs Sentiment, Distribution of sentiments across the year etc.
+
+     
---
+++ sentiment-main/pages/Data_Insights.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
+
+from logger import setup_logger 
+Logger = setup_logger(logger_file="sentiments")
+    
+# Bar Chart: Distribution of Overall Feelings
+def plot_overall_feelings(df):
+    try:
+        counts = df['overall'].value_counts()
+        fig = go.Figure(go.Bar(x=counts.index, y=counts.values))
+        fig.update_layout(
+            title='Distribution of Overall Feelings',
+            xaxis_title='Overall Feeling',
+            yaxis_title='Count',
+            title_x = 0.3,
+            xaxis={'categoryorder':'total descending'}
+        )
+        return fig
+    except Exception as e:
+        Logger.error(f"Error plotting overall feelings: {e}")
+        return None
+
+
+# Box Plot: Distribution of Sentiment Scores
+def plot_sentiment_score_distribution(df):
+    try:
+        fig = px.violin(df, y='sentiment score', box=True, points="all", title='Distribution of Sentiment Scores')
+        fig.update_layout(title_x=0.3, yaxis_title='Sentiment Score')
+        return fig
+    except Exception as e:
+        Logger.error(f"Error plotting sentiment score distribution: {e}")
+        return None
+
+
+# Dataframe: Distribution of Products against Sentiment
+def plot_product_sentiment(df):
+    try:
+        sentiment_counts = df.groupby('product_name')['overall'].value_counts().unstack(fill_value=0)
+        unique_products = df.drop_duplicates(subset='product_name', keep='first')['product_name']
+
+        # Filter the sentiment counts for unique products
+        unique_sentiment_counts = sentiment_counts.loc[unique_products]
+
+        return unique_sentiment_counts
+    except Exception as e:
+        Logger.error(f"Error plotting product sentiment: {e}")
+        return None
+
+# Sentiment across different months
+def generate_graph(df):
+    try:
+        month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        df['month'] = pd.Categorical(df['month'], categories=month_order, ordered=True)
+        
+        # Group by month and calculate average sentiment score
+        monthly_sentiment = df.groupby('month')['sentiment score'].mean().reset_index()
+        
+        # Group by month and sentiment and calculate counts
+        sentiment_counts = df.groupby(['month', 'overall']).size().reset_index(name='Count')
+        fig = go.Figure()
+        # Add trace for average sentiment score
+        fig.add_trace(go.Scatter(x=monthly_sentiment['month'], y=monthly_sentiment['sentiment score'], mode='lines+markers', name='Average Sentiment Score'))
+        for label in sentiment_counts['overall'].unique():
+            df_filtered = sentiment_counts[sentiment_counts['overall'] == label]
+            fig.add_trace(go.Scatter(x=df_filtered['month'], y=df_filtered['Count'], mode='lines+markers', name=label))
+        
+        fig.update_layout(
+            title='Overall Sentiment Across Months',        
+            title_x=0.3, 
+            xaxis_title='Month',
+            yaxis_title='Count / Average Score',
+            legend_title='Legend'
+        )
+        return fig
+    except Exception as e:
+        Logger.error(f"Error generating graph: {e}")
+        return None
+
+def main():
+    st.image("https://www.shutterstock.com/image-vector/application-laptop-business-graph-analytics-600nw-1228814305.jpg", use_column_width=True)
+    st.title("Business Data Insights")
+
+    csv_file = st.file_uploader("Please upload Your Updated Business' Reviews", type=["csv"])
+
+    if csv_file is None:
+        st.warning("Please upload an updated CSV file.")
+        return
+    try:
+        df = pd.read_csv(csv_file, parse_dates=['date'], date_format='%Y-%m-%d')
+    except (ValueError, KeyError):
+        st.error("Error: Invalid date format in the CSV file. Please ensure the date format is 'YYYY-MM-DD'.")
+        return
+
+    st.title('Review Insights')
+    st.sidebar.title('Filter Graph Options')
+
+    selected_insights = st.sidebar.multiselect('Select Insights', ['Overall Sentiment Across Months',
+                                                                    'Distribution of Overall Feelings',
+                                                                    'Distribution of Sentiment Scores',
+                                                                    'Distribution of Sentiment against Product'
+                                                                    ])
+
+    if 'Distribution of Overall Feelings' in selected_insights:
+        fig1 = plot_overall_feelings(df)
+        if fig1 is not None:
+            st.plotly_chart(fig1)
+
+    if 'Overall Sentiment Across Months' in selected_insights:
+        fig2 = generate_graph(df)
+        if fig2 is not None:
+            st.plotly_chart(fig2)
+
+    if 'Distribution of Sentiment Scores' in selected_insights:
+        fig3 = plot_sentiment_score_distribution(df)
+        if fig3 is not None:
+            st.plotly_chart(fig3)
+            
+    if 'Distribution of Sentiment against Product' in selected_insights:
+        df_sentiments = plot_product_sentiment(df)
+        if df_sentiments is not None:
+            st.dataframe(df_sentiments)
+
+if __name__ == '__main__':
+    main()
